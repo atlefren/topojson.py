a7fde5632d12786f86c0574d83c57d50ddc5148c
Atle Frenvik Sveen <atle.sveen@gmail.com>
2014-09-11 16:46:49 +0200

    linting and cleanup, take 1

--- topojson/utils.py
+++ topojson/utils.py
@@ -6,14 +6,23 @@ def point_compare(a, b):
 #        float(p[0]), float(p[1])
 #    except (TypeError, IndexError):
 #        return False
-is_point = lambda x : isinstance(x,list) and len(x)==2
+
+is_point = lambda x: isinstance(x, list) and len(x) == 2
+
+
 class Strut(list):
-    def __init__(self,ite=[]):
-        self.index=0
-        list.__init__(self,ite)
+    def __init__(self, ite=[]):
+        self.index = 0
+        list.__init__(self, ite)
+
+
 def is_infinit(n):
-    return abs(n)==float('inf')
+    return abs(n) == float('inf')
+
+
 E = 1e-6
+
+
 def mysterious_line_test(a, b):
     for arg in (a, b):
         if not isinstance(arg, list):
